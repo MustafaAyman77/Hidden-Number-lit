47fa9b3adf71c58b2caffc81256abc457c24c0ff
ViewCode15 <98029827+MustafaAyman77@users.noreply.github.com>
2026-08-09 01:49:37 +0300

    Update version code and version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.hiddennumber.game"
     minSdk = 24
     targetSdk = 36
-    versionCode = 10031
-    versionName = "1.0.31"
+    versionCode = 10069
+    versionName = "1.0.69"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
